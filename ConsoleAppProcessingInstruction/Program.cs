f43098aa39ac6a73ea2483067cffaed6fe9c8b9b
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:19:34 +0200

    ProcessingInstructions

--- ConsoleAppProcessingInstruction/Program.cs
+++ ConsoleAppProcessingInstruction/Program.cs
@@ -6,7 +6,33 @@ namespace ConsoleAppProcessingInstruction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press an arrow key! Press q to quit.");
+
+            var f = @"H:\documents\INFORMATICA\instructions.txt";
+
+            ConsoleKey k = ConsoleKey.Escape;
+
+
+            while (k != ConsoleKey.Q)
+            {
+                k = Console.ReadKey().Key;
+                switch (k)
+                {
+                    case ConsoleKey.UpArrow:
+                        System.IO.File.WriteAllText(f, "UP");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        System.IO.File.WriteAllText(f, "DOWN");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        System.IO.File.WriteAllText(f, "LEFT");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        System.IO.File.WriteAllText(f, "RIGHT");
+                        break;
+                }
+            }
         }
     }
+
 }
